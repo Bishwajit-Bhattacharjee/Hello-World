91330df5a97386921de68a81bc1e1dc384e63175
Bishwajit-Bhattacharjee <bishwajit0020@gmail.com>
2018-09-19 17:00:53 +0600

    Update Lightoj1129.cpp

--- Lightoj1129.cpp
+++ Lightoj1129.cpp
@@ -1,130 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int const CNT = 52;
+ 
 struct node{
-	int hasEnd;
-	node* edge[CNT];
-
-	node(){
-		hasEnd = 0;
-		for(int i = 0 ; i < CNT; i++)
-			edge[i] = 0;
-	}
+    bool hasEnd;
+    int prefCnt;
+    node* edge[10];
+ 
+    node(){
+        hasEnd = prefCnt = 0;
+        for(int i = 0 ; i < 10 ; i++)
+            edge[i] = 0;
+    }
 };
-
-int Map(char ch)
-{
-	if(ch >= 'a' and ch <= 'z')
-		return 26 + (ch - 'a');
-	return ch - 'A';
-}
-
-void modify(string& str)
-{
-	if(str.size() == 1) return;
-	priority_queue < char > pq;
-	for(int i = 1; i + 1 < str.size(); i++){
-		pq.push(str[i]);
-	}
-	for(int i = 1; i + 1 < str.size(); i++){
-		str[i] = pq.top();
-		pq.pop();
-	}
-
-}
 node* root ;
-
-void insert(string &str)
+ 
+bool insert(string &str)
 {
-	node* cur = root;
-	
-	for(int i = 0; i < str.size(); i++)
-	{
-		int id = Map(str[i]);
-		if(cur->edge[id] == 0){
-			cur->edge[id] = new node();
-		}
-		cur = cur->edge[id] ;
-		
-	}
-	cur->hasEnd++;
-
-}
-
-int query(string &str){
-	node* cur = root;
-
-	for(int i = 0 ; i < str.size(); i++){
-		int id = Map(str[i]);
-		if(cur->edge[id] == 0) return 0;
-		cur = cur->edge[id];
-	}
-	return cur->hasEnd;
+    node* cur = root;
+   
+    for(int i = 0; i < str.size(); i++)
+    {
+        if(cur->edge[str[i] -'0'] == 0){
+            cur->edge[str[i]-'0'] = new node();
+        }
+        if(cur->hasEnd){
+            return 0;
+        }
+        cur->prefCnt++;
+        cur = cur->edge[str[i]-'0'] ;
+    }
+    if(cur->prefCnt){
+        return 0;
+    }
+    cur->hasEnd = 1;
+   
+ 
+    return 1;
 }
 void del(node* now)
 {
-	node* cur = now;
-	for(int i =0; i < CNT ; i++){
-		if(cur->edge[i]){
-			del(cur->edge[i]);
-		}
-	}
-	delete cur;
+    node* cur = now;
+    for(int i =0; i < 10; i++){
+        if(cur->edge[i]){
+            del(cur->edge[i]);
+        }
+    }
+    delete cur;
 }
-//char dick[110], buf[4], input[(int)1e5 + 100];
-
+ 
 int main()
 {
-	#ifndef ONLINE_JUDGE
-		freopen("in.txt","r",stdin);
-		freopen("out.txt","w",stdout);
-	#endif
-
-	int t, c = 0;
-	scanf("%d",&t);
-
-	while(t--)
-	{
-		root = new node();
-		int n;
-		scanf("%d",&n);
-		//cin >> n;
-
-		for(int i = 0; i < n;i++){
-			string s;
-				
-			modify(s);
-			//cout << s << endl;
-			insert(s);
-			
-		}
-		printf("Case %d:\n",++c);
-
-		//cout << "Case " << ++c << ":" << '\n';
-	 	int m;
-	 	cin >> m;
-	 	//cout << m << endl;
-	 	cin.ignore();
-
-	 	while(m--){
-	 		int ans = 1;
-	 		string input;
-	 		getline(cin,input);
-	 		//cout << input << endl;
-	 		stringstream srm(input);
-	 		string tmp;
-	 		while(srm >> tmp){
-	 			//cout << tmp << endl;
-	 			modify(tmp);
-	 			//cout << query(tmp) << endl;
-
-	 			ans *= query(tmp);
-	 		}
-	 		printf("%d\n",ans); //;
-	 	}
-		//printf("Case %d: %d\n",++c,ans);
-		del(root);
-	}
-	return 0;
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+ 
+    int t, c = 0;
+    cin >> t;
+    while(t--)
+    {
+        root = new node();
+        int n;
+        scanf("%d",&n);
+        bool f = 1;
+        for(int i = 0;i < n; i++)
+        {
+            string s;
+            cin >> s;
+            f &= insert(s);
+        }
+        printf("Case %d: %s\n",++c,f?"YES":"NO");
+        del(root);
+    }
+    return 0;
 }
