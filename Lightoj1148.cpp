007d26db5f08f66c63d4b9d176b9680bd803b85b
Bishwajit-Bhattacharjee <bishwajit0020@gmail.com>
2018-09-19 19:57:30 +0600

    Update Lightoj1148.cpp

--- Lightoj1148.cpp
+++ Lightoj1148.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
 #ifndef ONLINE_JUDGE
-	freopen("in.txt","r", stdin) ;
-	freopen("out.txt","w", stdout);
+    freopen("in.txt","r", stdin) ;
+    freopen("out.txt","w", stdout);
 #endif
-
-	int t, c =0 ;
-	scanf("%d",&t) ;
-
-	while(t--)
-	{
-		int n;
-		scanf("%d",&n) ;
-		vector < int > v(n);
-		map < int , int > freq;
-		for(int i =0; i < n; i++)
-			scanf("%d",&v[i]), freq[v[i]]++;
-
-		int ans =0 ;
-		map < int ,int >::iterator it = freq.begin();
-		for( ; it != freq.end(); it++) 
-		{
-			if(it->second)
-			{
-				int tmp = (it->second + it->first ) / ( it->first + 1) ;
-				ans += tmp * (it->first + 1) ;
-			}
-		}
-		
-		printf("Case %d: %d\n",++c,ans);
-	}
-	return 0 ;
+ 
+    int t, c =0 ;
+    scanf("%d",&t) ;
+ 
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n) ;
+        vector < int > v(n);
+        vector < int > freq(1000010, 0) ;
+ 
+        for(int i =0; i < n; i++)
+            scanf("%d",&v[i]), freq[v[i]]++;
+ 
+        int ans =0 ;
+        for(int i = 0; i < 1000010; i++)
+        {
+            if(freq[i])
+            {
+                int tmp = (freq[i] + i ) / ( i + 1) ;
+                ans += tmp * (i + 1) ;
+            }
+        }
+       
+        printf("Case %d: %d\n",++c,ans);
+    }
+    return 0 ;
 }
